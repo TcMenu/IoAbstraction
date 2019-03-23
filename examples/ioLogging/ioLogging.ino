6c177b8d4120fe5182eaf8772a531b59c8c30421
Dave Cherry <dave@nutricherry.com>
2019-03-23 10:43:43 +0000

    #48 put very basic logging funcitonality into library (compiles out when not enabled)

---
+++ examples/ioLogging/ioLogging.ino
@@ -0,0 +1,38 @@
+/** 
+ * Advanced feature of library.
+ * 
+ * This example shows how to use the very simple logging that's built into IoAbstraction.
+ * To enable logging open IoLogging.h in the IoAbstraction directory and uncomment
+ * #define IO_LOGGING_DEBUG
+ * 
+ * This logging is only complied in when the above define is set, if it is not set then
+ * the logging is completely removed.
+ */
+#include <IoAbstraction.h>
+
+char sz[] = {"hello world"};
+
+void setup() {
+    while(!Serial);
+    Serial.begin(115200);
+
+    // write a string entry that is applied with F(..) so in progmem on AVR
+    // with an integer second value.
+    serdebugF2("In setup function - A0=", analogRead(A0));
+
+    taskManager.scheduleFixedRate(10, [] {
+        // write values to log in HEX - first parameter is wrapped in F(..) using the F variant
+        serdebugFHex2("Two Values in hex: ", 0xFADE, 0xFACE);
+        serdebugFHex("One Values in hex: ", 0xFADE);
+
+        // the F variant always tries to use F(..) to save ram on the first parameter on AVR
+        serdebugF("String in flash");
+        
+        // this version does not use F(..)
+        serdebug(sz); 
+    }, TIME_SECONDS);
+}
+
+void loop() {
+    taskManager.runLoop();
+}
--- src/IoLogging.h
+++ src/IoLogging.h
@@ -9,26 +9,29 @@
  */
 
 // comment out the line below to stop logging
-#define IO_LOGGING_DEBUG
-
-// if you want to log somewhere else than Serial, change below.
-#define LoggingPort Serial
+//#define IO_LOGGING_DEBUG
 
 #ifdef IO_LOGGING_DEBUG
-#define serdebugF(x) LoggingPort.println(F(x));
-#define serdebugF2(x1, x2) LoggingPort.print(F(x1)); LoggingPort.println(x2);
-#define serdebugF3(x1, x2, x3) LoggingPort.print(F(x1)); LoggingPort.print(x2); LoggingPort.print(' '); LoggingPort.println(x3);
-#define serdebugFHex(x1, x2) LoggingPort.print(F(x1)); LoggingPort.println(x2, HEX);
-#define serdebugFHex2(x1, x2, x3) LoggingPort.print(F(x1)); LoggingPort.print(x2, HEX) LoggingPort.print(','); LoggingPort.println(x3, HEX);
-#define serdebug(x) LoggingPort.println(x);
-#define serdebug2(x1, x2) LoggingPort.print(x1); LoggingPort.println(x2);
-#define serdebugHex(x1, x2) LoggingPort.print(x1); LoggingPort.println(x2, HEX);
+
+// change to log elsewhere than Serial.
+#define LoggingPort Serial
+
+#define logTime(title) LoggingPort.print(millis());LoggingPort.print(':');LoggingPort.print(title)
+#define serdebugF(x) logTime(F(x));LoggingPort.println();
+#define serdebugF2(x1, x2) logTime(F(x1)); LoggingPort.println(x2);
+#define serdebugF3(x1, x2, x3) logTime(F(x1)); LoggingPort.print(x2); LoggingPort.print(' '); LoggingPort.println(x3);
+#define serdebugFHex(x1, x2) logTime(F(x1)); LoggingPort.println(x2, HEX);
+#define serdebugFHex2(x1, x2, x3) logTime(F(x1)); LoggingPort.print(x2, HEX); LoggingPort.print(','); LoggingPort.println(x3, HEX);
+#define serdebug(x) logTime(x);LoggingPort.println();
+#define serdebug2(x1, x2) logTime(x1); LoggingPort.println(x2);
+#define serdebugHex(x1, x2) logTime(x1); LoggingPort.println(x2, HEX);
 #else
 // all loging to no operations (commenting out the above define of IO_LOGGING_DEBUG to remove in production builds).
 #define serdebugF(x) 
 #define serdebugF2(x, y) 
 #define serdebugF3(x, y, z) 
 #define serdebugFHex(x, y) 
+#define serdebugFHex2(x, y, z) 
 #define serdebug(x) 
 #define serdebug2(x, y) 
 #define serdebugHex(x, y) 
