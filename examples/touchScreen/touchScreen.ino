cf8107e152d8fdf70305c870561eeeab81a89617
dave <dave@thecoderscorner.com>
2021-02-09 08:31:09 +0000

    Fixed touch screen example

--- examples/touchScreen/touchScreen.ino
+++ examples/touchScreen/touchScreen.ino
@@ -7,7 +7,8 @@
 #include <Arduino.h>
 #include <IoAbstraction.h>
 #include <ResistiveTouchScreen.h>
-#include <Adafruit_ILI9341.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
 
 #define YPOS_PIN 32
 #define XNEG_PIN 33
@@ -15,33 +16,39 @@
 #define YNEG_PIN 0
 #define MY_CS 22
 #define MY_DC 21
+#define MY_RST 23
 
-ArduinoAnalogDevice analogDevice;
-ValueStoringResistiveTouchScreen* touchScreen;
-Adafruit_ILI9341 gfx(MY_CS, MY_DC);
+using namespace iotouch;
+
+// the touch screen itself
+ValueStoringResistiveTouchScreen touchScreen(XPOS_PIN, XNEG_PIN, YPOS_PIN, YNEG_PIN, BaseResistiveTouchScreen::PORTRAIT);
+
+// couple of display options here, anything with a touch interface attached!
+Adafruit_PCD8544 gfx(MY_CS, MY_DC, MY_RST);
+//Adafruit_ILI9341 gfx(MY_CS, MY_DC);
+
 int oldX = 0, oldY = 0;
 
 void setup() {
     Serial.begin(115200);
-    touchScreen = new ValueStoringResistiveTouchScreen(&analogDevice, internalDigitalIo(), XPOS_PIN, XNEG_PIN, YPOS_PIN, YNEG_PIN, BaseResistiveTouchScreen::PORTRAIT);
     // step 1. run with calibration off and get the actual min and max values if corrections need to be made
     // step 2. put the corrections into the value below, xmin, xmax, ymin, ymax and try the program again.
-    touchScreen->calibrateMinMaxValues(0.15F, 0.75F, 0.06F, 0.91F);
-    touchScreen->start();
+    touchScreen.calibrateMinMaxValues(0.15F, 0.75F, 0.06F, 0.91F);
+    touchScreen.start();
 
     SPI.begin();
-    gfx.begin(20000000);
-    gfx.fillScreen(ILI9341_BLACK);
+    gfx.begin();
+    gfx.fillScreen(0);
 
     taskManager.scheduleFixedRate(500, [] {
-        serdebugF4("Touch: x, y, touched", touchScreen->getLastX(), touchScreen->getLastY(), touchScreen->getTouchPressure());
-        serdebugF2("touchMode: ", touchScreen->getTouchState());
+        serdebugF4("Touch: x, y, touched", touchScreen.getLastX(), touchScreen.getLastY(), touchScreen.getTouchPressure());
+        serdebugF2("touchMode: ", touchScreen.getTouchState());
 
-        if(touchScreen->getTouchState() == BaseResistiveTouchScreen::TOUCHED || touchScreen->getTouchState() == BaseResistiveTouchScreen::HELD) {
-            gfx.fillCircle(oldX, oldY, 16, ILI9341_BLACK);
-            oldX = touchScreen->getLastX() * 240.0F;
-            oldY = touchScreen->getLastY() * 320.0F;
-            gfx.fillCircle(oldX, oldY, 16, ILI9341_WHITE);
+        if(touchScreen.getTouchState() == TOUCHED || touchScreen.getTouchState() == HELD) {
+            gfx.fillCircle(oldX, oldY, 16, 0);
+            oldX = touchScreen.getLastX() * 240.0F;
+            oldY = touchScreen.getLastY() * 320.0F;
+            gfx.fillCircle(oldX, oldY, 16, 0xffff);
         }
     });
 }
